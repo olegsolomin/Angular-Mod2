0d6d1919f544cb35691e40f53ce8eaa410b1ba27
Oleg Solomin <solomin.o.n@gmail.com>
2021-04-12 23:17:46 +0300

    made 8 steps

---
+++ app.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+
+        .controller('ShoppingListCheckOff', ShoppingListCheckOff);
+
+    ShoppingListCheckOff.$inject = ['$scope'];
+    function ShoppingListCheckOff($scope) {
+        
+        }
+        
+
+})();
