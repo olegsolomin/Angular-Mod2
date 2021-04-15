e830b574d006c70050293acdd211b5431897f48a
olegsolomin <solomin.o.n@gmail.com>
2021-04-15 15:10:04 +0300

    almost working

--- app.js
+++ app.js
@@ -10,57 +10,45 @@
     function ToBuyController(ShoppingListCheckOffService) {
         var wtbctrl = this;
         wtbctrl.items = ShoppingListCheckOffService.getItemsWTB();
-        wtbctrl.boughtItem = function (index) {
-        ShoppingListCheckOffService.boughtItem(index);
+        wtbctrl.boughtItem = function (itemIndex) {
+        ShoppingListCheckOffService.boughtItem(itemIndex);
     };
     }
     //abctrl - already bought
     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
     function AlreadyBoughtController(ShoppingListCheckOffService) {
        var abctrl = this;
-        abctrl.items = ShoppingListCheckOffService.getItemsAB()
+        abctrl.items = ShoppingListCheckOffService.getItemsAB();
     }
 
     function ShoppingListCheckOffService() {
         var service = this;
 
         // List of shopping items to buy - WTB
-        var BuyList = [
+        var WTBlist = [
             {name: "cookies", quantity: 10},
             {name: "tea", quantity: 5},
             {name: "frozen pizza", quantity: 3},
             {name: "cheese", quantity: 2},
             {name: "onion", quantity: 3},
         ];
-        var WTBlist = BuyList;
-        // get wtbuy list
-        service.getWTBlist = function () {
-            return WTBlist;
-        };
-        // move to bought
-        service.boughtItem = function (itemIndex) {
-            var item = WTBlist[itemIndex];
-            service.addBoughtItem(item);
-            removeItemFromWTB(itemIndex);
-        };
-        // List of bought items
-        var bought = [];
-        var ABlist = bought;
-        // remove from WTB
-        service.removeItemFromWTB = function (itemIndex) {
-            WTBlist.splice(itemIndex, 1);
-        };
+        var ABlist = [];
+       
         // WTB list
         service.getItemsWTB = function () {
             return WTBlist;
         };
+
+        // move to bought
+        service.boughtItem = function (itemIndex) {
+            var item =  WTBlist.splice(itemIndex, 1);
+            ABlist.push(item);
+        };
+        
         //Already Bought list
         service.getItemsAB = function () {
             return ABlist;
         };
-        //add to AB list
-        service.addItemToAB = function (item) {
-            ABlist.push(item);
-        };
+    
     }
 })();
